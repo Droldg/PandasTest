b100aad105583f18b76d184db7dee6e2cf64f11e
Droldg <daro23ac@student.cbs.dk>
2025-01-24 01:23:46 +0100

    Fjernet overflåd og fixet gitignore

--- main.py
+++ main.py
@@ -5,28 +5,17 @@ db = pd.read_csv('FINA2012.csv', sep=';')
 search_string = "2001" 
 kolonne = "Født"
 if search_string.isnumeric():
-    filtered_rows = db[db[kolonne] == search_string]
+    numeric_value = int(search_string)
+    filtered_rows = db[db[kolonne] == numeric_value]
 else:
-    
     filtered_rows = db[db[kolonne].str.contains(search_string)]
 
 
-
+#debug
 #print(filtered_rows)
 
 df = pd.DataFrame(filtered_rows)
-def export_to_csv(df: pd.DataFrame, filename: str = "output.csv") -> None:
-    """
-    Gemmer en Pandas DataFrame som en CSV-fil.
 
-    :param df: DataFrame, der skal gemmes
-    :param filename: Filnavn/sti til den resulterende CSV
-    """
-    df.to_csv(filename, index=False, encoding="utf-8")
-    print(f"CSV gemt som: {filename}")
-    
-
-#export_to_csv(df, "OutputData.csv")
 
 
 def export_to_csv_semicolon(df: pd.DataFrame, filename: str = "output.csv") -> None:
@@ -36,7 +25,7 @@ def export_to_csv_semicolon(df: pd.DataFrame, filename: str = "output.csv") -> N
 
 
 
-export_to_csv_semicolon(df, f"{kolonne}+{search_string}.csv")
+export_to_csv_semicolon(df, f"./output/{kolonne}+{search_string}.csv")
 
 #print(filtered_rows)
 
